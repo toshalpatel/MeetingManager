912b6dd0895ea6d20f68f3c51c683fc9068fdcf3
Toshal Patel <toshaltpatel@gmail.com>
2017-06-20 15:06:10 +0530

    Add files via upload

---
+++ examples/src/main/java/com/ibm/watson/developer_cloud/android/examples/SetTime.java
@@ -0,0 +1,18 @@
+package com.ibm.watson.developer_cloud.android.examples;
+
+public class SetTime
+{
+    String time;
+
+    public SetTime(String time) {
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
