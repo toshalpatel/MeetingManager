7a72884dfbeaec1cfcb25d126df837e0bfe656bb
Toshal Patel <toshaltpatel@gmail.com>
2017-06-20 15:05:51 +0530

    Add files via upload

---
+++ examples/src/main/java/com/ibm/watson/developer_cloud/android/examples/SetDate.java
@@ -0,0 +1,21 @@
+package com.ibm.watson.developer_cloud.android.examples;
+
+/**
+ * Created by Megha on 5/17/2017.
+ */
+public class SetDate
+{
+    String date;
+
+    public SetDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
